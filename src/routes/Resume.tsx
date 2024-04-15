f63ec4a2d69aab032e1ac7b9b0775ef8e6ef8344
Narnia <princessnarnia1@gmail.com>
2024-04-15 17:02:32 -0400

    Add content for resume and github

--- src/routes/Resume.tsx
+++ src/routes/Resume.tsx
@@ -1,8 +1,18 @@
 import NavbarRoute from './templates/NavbarRoute';
 import bg from '../img/bg-resume.jpg'
+import { Col, Container, Row } from 'react-bootstrap';
+import StyledCol from './templates/StyledCol';
+import '../css/page_resume.css'
 
 const Resume = () => <NavbarRoute BackgroundImage={bg} Element={
-  <>Resume</>
+  <Container className='resume'>
+      <Row>
+        <StyledCol lg={12}>
+          <h1>Where can I find your resume?</h1>
+          Feel free to shoot me a message privately and I will send it to you via e-mail.
+        </StyledCol>
+      </Row>
+    </Container>
 } />
 
 export default Resume
